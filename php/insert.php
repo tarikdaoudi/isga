8f655b463b17abe95a70947ce1a63f4ddc5f4ca2
tarikdaoudi <d.tarik.itm@gmail.com>
2024-01-13 14:31:13 +0100

    Initial add

---
+++ php/insert.php
@@ -0,0 +1,18 @@
+<?php
+$host = 'db';
+$dbn = 'php-app';
+$user = 'USER';
+$pass = 'PASS';$conn = new mysqli($host, $user, $pass,$dbn);if(isset($_POST['submit']))
+{
+  $name = $_POST['name'];
+  $email = $_POST['email'];
+  $mobile = $_POST['mobile'];
+  $sql = "INSERT INTO users (name,email,mobile)VALUES    ('$name','$email','$mobile')";if (mysqli_query($conn, $sql)) {
+echo "New record has been added successfully !";
+} 
+else {
+echo "Error: " . $sql . ":-" . mysqli_error($conn);
+}
+mysqli_close($conn);
+}
+?>
